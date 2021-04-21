12a541c520c5479eb66e51c76e6d9f136523a195
LucasLeibs <dr.lucasleibs@gmail.com>
2021-04-21 15:14:30 -0700

    adding graph implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [asset, setAsset] = useState('BTC');
+ const [assetInfo, setAssetInfo] = useState({
+   name: '',
+   price: 0
+ })
+  
+
+  useEffect(() => {
+    fetch(`https://rest.coinapi.io/v1/assets/${asset}?apikey=3DB0B8D6-BD54-4D47-9F9D-B276C963304E`)
+    .then(res => res.json())
+    .then(data => setAssetInfo({
+      name: data[0].name,
+      price: data[0].price_usd,
+    }))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{assetInfo.name},{assetInfo.price}</h1>
     </div>
   );
 }
