268c32155e7b4269dffd0167b6ee917abd254797
LucasLeibs <dr.lucasleibs@gmail.com>
2021-04-22 14:26:17 -0700

    implemented percent change to toggle green or red if pos or neg

--- src/Asset.js
+++ src/Asset.js
@@ -2,15 +2,41 @@ import React from 'react'
 import './styles/styles.css'
 
 export default function Asset(props) {
+    const numberFormat = (number) => {
+      return  number.toLocaleString()
+    }
+
+    const percentChange = (percent) => {
+        
+     let s = percent.toString()
+    return  s.includes("-") ? <p className="price-change-neg">{percent}%</p> : <p className="price-change-neg">{percent}%</p>
+    }
     return (
-        <div className="asset-table">
+        <table className="asset-table">
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>24hr %</th>
+                <th>Market Cap</th>
+                
+            </tr>
             {props.assets.map(asset => (
-                <div className="asset-data">
-                    <img src={asset.image}></img>
-                    <h1>{asset.id}</h1>
-                    
-                </div>
+                
+                <tr>
+                    <td className="name">
+                    <p><img src={asset.image}></img>{asset.id}</p>
+                    </td>
+                    <td>
+                        <p> $ {numberFormat(asset.current_price)}</p>
+                    </td>
+                    <td>
+                        {percentChange(asset.price_change_percentage_24h)}
+                    </td>
+                    <td>
+                        {numberFormat(asset.market_cap)}
+                    </td>
+                </tr>
             ))}
-        </div>
+        </table>
     )
 }
