d297b612491bb3345c7bd4489841fae08630ddbd
LucasLeibs <dr.lucasleibs@gmail.com>
2021-04-22 12:57:30 -0700

    adding 50 assets on load of page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/styles.css';
 import {useEffect, useState} from 'react';
 import Search from './Search'
 
 function App() {
   
- const [assetInfo, setAssetInfo] = useState({
-   name: '',
-   price: 0,
-   volume1day: '',
-   image: ''
- })
- const [icon, setIcon] = useState('');
+ const [assets, setAssets] = useState([])
+
+ useEffect(() => {
+  fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false
+  `)
+  .then(res => res.json())
+  .then(data => setAssets(data.map(asset => asset)));
+ },[])
   
 
   const query = (asset) => {
-    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${asset}&order=market_cap_desc&per_page=100&page=1&sparkline=false
-    `)
-    .then(res => res.json())
-    .then(data => setAssetInfo({
-      name: data[0].id,
-      price: data[0].current_price,
-      image: data[0].image,
-
-    }));
+    
   }
  
   //  const logoSearch = (input) => {
@@ -31,13 +24,14 @@ function App() {
   //   .then(res => res.json())
   //   .then(data => setIcon(data.filter(asset => asset.asset_id == input)))
   //  }
-   
+   console.log(assets)
   return (
  
-    <div className="App">
+    <div className="page-container">
           <Search query={query}></Search>
-     <h1>{assetInfo.name}<br></br>Price: {assetInfo.price} <br></br>1 Day Volume: {assetInfo.volume1day}, </h1>
-     <img src={assetInfo.image}></img>
+          <div className="asset-table">
+            
+          </div>
  
     </div>
   )
