195e65437ed17991b957baf3531e7522b07b8d92
LucasLeibs <dr.lucasleibs@gmail.com>
2021-05-03 17:57:55 -0700

    working on graph page

--- src/Chart.js
+++ src/Chart.js
@@ -6,6 +6,34 @@ import { XYPlot, LineSeries, Hint, XAxis, YAxis } from "react-vis";
 import "../node_modules/react-vis/dist/style.css";
 import { useLocation } from "react-router-dom";
 import moment from "moment";
+const percentChangeIcons = [
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="down-arrow"
+      className="h-6 w-6"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
+        clipRule="evenodd"
+      />
+    </svg>,
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="up-arrow"
+      className="h-5 w-5"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
+        clipRule="evenodd"
+      />
+    </svg>,
+  ];
 
 export default function Chart() {
   const [hintValue, setHint] = useState("");
@@ -47,8 +75,20 @@ export default function Chart() {
       : "red";
   };
 
-  const dateValues = () => {
-    //  console.log(state.prices.map(data => data[0][0]))
+  const percentChange = (percent) => {
+    let s = percent.toString();
+
+    return s.includes("-") ? (
+      <div className="p-change-neg">
+        {percentChangeIcons[0]}
+        {s.replace("-", "").slice(0, 4)}%
+        </div>
+    ) : (
+      <div className="p-change-pos">
+        {percentChangeIcons[1]}
+        {s.slice(0, 5)}%
+      </div>
+    );
   };
 
   const dailyChart = () => {
@@ -65,8 +105,17 @@ export default function Chart() {
   };
   return (
     <div className="chart-page-container">
+      
       <div className="chart-asset-info">
-        <h1>{state.asset.id.replace(state.asset.id.charAt(0), state.asset.id.charAt(0).toUpperCase())}</h1>
+      <div className="current-price">
+     <h1>Current price: <data>$ {state.asset.current_price.toLocaleString()}</data></h1>
+     {percentChange(state.asset.price_change_percentage_24h)}
+          </div>
+          <div className="chart-asset-header">
+          <img src={state.asset.image}></img>
+        <h1> {state.asset.id.replace(state.asset.id.charAt(0), state.asset.id.charAt(0).toUpperCase())}</h1>
+        <h1>{state.asset.symbol.toUpperCase()}</h1>
+        </div>
       </div>
       {showDaily === false ? (
           <div className="chart">
@@ -99,9 +148,9 @@ export default function Chart() {
           xType="time"
           onMouseLeave={_onMouseLeave}
           height={500}
-          width={800}
+          width={700}
         >
-          <YAxis title="Price"></YAxis>
+          <YAxis tickFormat={(v) => `${v.toString().slice(0, 2)}K`} title="Price"></YAxis>
           <XAxis title="Time"></XAxis>
           <LineSeries
             onNearestXY={_onNearestX}
--- src/IndividualAsset.js
+++ src/IndividualAsset.js
@@ -1,161 +1,202 @@
-import React, {useState, useEffect} from 'react'
-import '../node_modules/react-vis/dist/style.css';
-import {XYPlot, LineSeries} from 'react-vis';
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import Popover from 'react-bootstrap/Popover'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import 'react-circular-progressbar/dist/styles.css';
-import moment from 'moment';
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import "../node_modules/react-vis/dist/style.css";
+import { XYPlot, LineSeries } from "react-vis";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import "react-circular-progressbar/dist/styles.css";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
 const percentChangeIcons = [
-    <svg xmlns="http://www.w3.org/2000/svg" id="down-arrow" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z" clipRule="evenodd" />
-</svg>,
-<svg xmlns="http://www.w3.org/2000/svg" id="up-arrow" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clipRule="evenodd" />
-</svg>
-]
-export default function IndividualAsset({asset}) {
-    
-    const [prices, setPrices] = useState([]);
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="down-arrow"
+    className="h-6 w-6"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
+      clipRule="evenodd"
+    />
+  </svg>,
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="up-arrow"
+    className="h-5 w-5"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
+      clipRule="evenodd"
+    />
+  </svg>,
+];
+export default function IndividualAsset({ asset }) {
+  const [prices, setPrices] = useState([]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
+  useEffect(() => {
+    let coinPrices = localStorage.getItem("prices");
 
-useEffect(() => {
-    let coinPrices = localStorage.getItem("prices")
-//   coinPrices ? : fetchData(asset.id)
-  if (coinPrices) {
+    if (coinPrices) {
       coinPrices = JSON.parse(coinPrices);
-      setPrices(coinPrices)
-  } else {
-    fetch(`https://api.coingecko.com/api/v3/coins/${asset.id}/market_chart?vs_currency=usd&days=7&interval=hourly`)
-    .then(res => res.json())
-    .then(data => { 
-        setPrices(data.prices);
-        localStorage.setItem("coinPrices", JSON.stringify(data.prices))
-       
-    })
-     
-  }
-    
-   
-}, [])
-// const fetchData = (asset) => {
-
-//     fetch(`https://api.coingecko.com/api/v3/coins/${asset}/market_chart?vs_currency=usd&days=7&interval=hourly`)
-//     .then(res => res.json())
-//     .then(data => { 
-//         setPrices(data.prices);
-//         localStorage.setItem(coinPrices, JSON.stringify(data.prices))
-       
-//     })
-
-// }
-// const capitalize = (asset) => {
-// return asset.charAt(0).toUpperCase() + asset.
-// }
-const loopData = () => {
-  
-    let data = []
-    for(let i = 0; i < prices.length; i++) {
-        
-        data.push({x:i, y:prices[i][1]})
+      setPrices(coinPrices);
+    } else {
+      fetch(
+        `https://api.coingecko.com/api/v3/coins/${asset.id}/market_chart?vs_currency=usd&days=7&interval=hourly`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setPrices(data.prices);
+          localStorage.setItem("coinPrices", JSON.stringify(data.prices));
+        });
     }
-   return data
- 
-  
-}
- const data = loopData() 
+  }, []);
 
+  const loopData = () => {
+    let data = [];
+    for (let i = 0; i < prices.length; i++) {
+      data.push({ x: i, y: prices[i][1] });
+    }
+    return data;
+  };
+  const data = loopData();
 
   const numberFormat = (number) => {
-    return  Math.floor(number).toLocaleString()
-  }
+    return Math.floor(number).toLocaleString();
+  };
 
   const percentChange = (percent) => {
-  
-   let s = percent.toString()
-   
-  return  s.includes("-") ? <p className="price-change-neg">{percentChangeIcons[0]}{s.replace("-", "").slice(0,4)}%</p> : <p className="price-change-pos">{percentChangeIcons[1]}{s.slice(0,5)}%</p>
-  }
+    let s = percent.toString();
+
+    return s.includes("-") ? (
+      <p className="price-change-neg">
+        {percentChangeIcons[0]}
+        {s.replace("-", "").slice(0, 4)}%
+      </p>
+    ) : (
+      <p className="price-change-pos">
+        {percentChangeIcons[1]}
+        {s.slice(0, 5)}%
+      </p>
+    );
+  };
   const graphColor = () => {
-     return prices[prices.length-1][1] > prices[0][1] ? "green" : "red"
-  }
+    return prices[prices.length - 1][1] > prices[0][1] ? "green" : "red";
+  };
 
   const percentSupply = (asset) => {
-      let percent = (asset.circulating_supply * 100) / asset.max_supply
-         
-        return `${percent.toString().slice(0,4)} %`
- 
-  }
+    let percent = (asset.circulating_supply * 100) / asset.max_supply;
+
+    return `${percent.toString().slice(0, 4)} %`;
+  };
 
   const popover = (
     <Popover id="popover-basic">
-     <Popover.Title><strong>{asset.id.replace(asset.id.charAt(0), asset.id.charAt(0).toUpperCase())}</strong></Popover.Title>
-     <Popover.Content>{percentSupply(asset)} of supply is in circulation</Popover.Content>
-     <Popover.Content>Circulating Supply: {numberFormat(asset.circulating_supply)} {asset.symbol.toUpperCase()}</Popover.Content>
-     <Popover.Content>Total: {numberFormat(asset.max_supply)} {asset.symbol.toUpperCase()}</Popover.Content>
+      <Popover.Title>
+        <strong>
+          {asset.id.replace(
+            asset.id.charAt(0),
+            asset.id.charAt(0).toUpperCase()
+          )}
+        </strong>
+      </Popover.Title>
+      <Popover.Content>
+        {percentSupply(asset)} of supply is in circulation
+      </Popover.Content>
+      <Popover.Content>
+        Circulating Supply: {numberFormat(asset.circulating_supply)}{" "}
+        {asset.symbol.toUpperCase()}
+      </Popover.Content>
+      <Popover.Content>
+        Total: {numberFormat(asset.max_supply)} {asset.symbol.toUpperCase()}
+      </Popover.Content>
     </Popover>
-  )
+  );
 
-    return (
-        <tr className="row">
-  
-        <td className="name">
-       
-        <p><img src={asset.image}></img>{asset.id.replace(asset.id.charAt(0), asset.id.charAt(0).toUpperCase())} <h3>{asset.symbol.toUpperCase()}</h3></p>
-        
-        </td>
-       
-        <td className="price">
-            <p> $ {asset.current_price.toLocaleString()}</p>
-        </td>
-        <td>
-            {percentChange(asset.price_change_percentage_24h)}
-        </td>
-        <td>
-            {numberFormat(asset.market_cap)}
-        </td>
-        <td className="supply">
-           <p>{numberFormat(asset.circulating_supply)} {asset.symbol.toUpperCase()}</p>
-           
-            <div className="circle-graph" style={{ width: 50, height: 50 }}>
-                {asset.max_supply == null ? <CircularProgressbar value={asset.circulating_supply} maxValue={asset.max_supply} text={'No Max'} styles={buildStyles({
-    
-    pathColor: `#d4d9d5`,
-    textColor: '',
-    trailColor: 'white',
-    backgroundColor: '#d4d9d5',
-  })}
-/>: <OverlayTrigger trigger="hover" placement="top" overlay={popover}><div><CircularProgressbar value={asset.circulating_supply} maxValue={asset.max_supply} text={percentSupply(asset)} styles={buildStyles({
-    
-    pathColor: `#0aa5c4`,
-    textColor: '',
-    trailColor: 'white',
-    backgroundColor: '#3e98c7',
-    pathTransitionDuration: 2,
-    transition: 'stroke-dashoffset 0.5s ease 0s',
-   
-  })}
-/></div></OverlayTrigger>}
+  return (
+    <tr className="row">
+      <td className="name">
+        <p>
+          <img src={asset.image}></img>
+          {asset.id.replace(
+            asset.id.charAt(0),
+            asset.id.charAt(0).toUpperCase()
+          )}{" "}
+          <h3>{asset.symbol.toUpperCase()}</h3>
+        </p>
+      </td>
 
+      <td className="price">
+        <p> $ {asset.current_price.toLocaleString()}</p>
+      </td>
+      <td>{percentChange(asset.price_change_percentage_24h)}</td>
+      <td>{numberFormat(asset.market_cap)}</td>
+      <td className="supply">
+        <p>
+          {numberFormat(asset.circulating_supply)} {asset.symbol.toUpperCase()}
+        </p>
 
-            </div>
-      
-        </td>
-        <td> 
-            <div>
-        <XYPlot height={100} width={200}><LineSeries color={prices.length > 0 ? graphColor() : 'blue'} className="line" data={data}/></XYPlot>
-        </div><Link to={{
-    pathname: `/charts/${asset.id}`,
-    state: { asset: asset,
-    prices: prices }
-  }}>charts</Link></td>
-     
+        <div className="circle-graph" style={{ width: 50, height: 50 }}>
+          {asset.max_supply == null ? (
+            <CircularProgressbar
+              value={asset.circulating_supply}
+              maxValue={asset.max_supply}
+              text={"No Max"}
+              styles={buildStyles({
+                pathColor: `#d4d9d5`,
+                textColor: "",
+                trailColor: "white",
+                backgroundColor: "#d4d9d5",
+              })}
+            />
+          ) : (
+            <OverlayTrigger trigger="hover" placement="top" overlay={popover}>
+              <div>
+                <CircularProgressbar
+                  value={asset.circulating_supply}
+                  maxValue={asset.max_supply}
+                  text={percentSupply(asset)}
+                  styles={buildStyles({
+                    pathColor: `#0aa5c4`,
+                    textColor: "",
+                    trailColor: "white",
+                    backgroundColor: "#3e98c7",
+                    pathTransitionDuration: 2,
+                    transition: "stroke-dashoffset 0.5s ease 0s",
+                  })}
+                />
+              </div>
+            </OverlayTrigger>
+          )}
+        </div>
+      </td>
+      <td>
+        <div>
+          <XYPlot height={100} width={200}>
+            <LineSeries
+              color={prices.length > 0 ? graphColor() : "blue"}
+              className="line"
+              data={data}
+            />
+          </XYPlot>
+        </div>
+        <Link
+          to={{
+            pathname: `/charts/${asset.id}`,
+            state: { asset: asset, prices: prices },
+          }}
+        >
+          charts
+        </Link>
+      </td>
     </tr>
-    
-    )
+  );
 }
