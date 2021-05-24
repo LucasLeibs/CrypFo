8de78f2acc20baf5df282bdd4a3b7410935871a6
LucasLeibs <dr.lucasleibs@gmail.com>
2021-05-24 15:30:18 -0700

    adding stat info for mobile

--- src/Chart.js
+++ src/Chart.js
@@ -345,7 +345,28 @@ const [hoveredNode, setHoveredNode] = useState(null)
           }
           
           </section>
-          
+          <div className="coin-stats">
+          <div className="stat">
+            <div className="desc">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd" />
+
+</svg>
+<h3>Market Cap</h3>
+</div>
+          <h3>{state.asset.market_cap.toLocaleString()}</h3>
+          </div>
+          <div className="stat">
+            <div className="desc">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+<h3>Market Rank</h3>
+</div>
+          <h3>{state.asset.market_cap_rank}</h3>
+          </div>
+          </div>
           </div>
         </MediaQuery>
       
--- src/Search.js
+++ src/Search.js
@@ -7,7 +7,7 @@ export default function Search(props) {
     return (
        
           <form onSubmit={e => { e.preventDefault(); }}>
-            <input placeholder="Search Cryptocurrency's" className="search-bar" onChange={(e) => props.search(e)}></input>
+            <input placeholder="Search Cryptocurrencies" className="search-bar" onChange={(e) => props.search(e)}></input>
             
             </form>
       
